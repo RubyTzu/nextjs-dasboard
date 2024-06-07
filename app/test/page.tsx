c11e6d827e9f3273ede6be1876746c364cf0eabe
Ruby Chen <ruby.chen@cheil.com>
2024-06-07 16:53:11 +0800

    feat: modified groups picture

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -11,8 +11,7 @@ export default async function Page() {
     const dogImageUrl = dogs.message;
     const cats = await getCats();
     const catImageUrl = cats[0].url;
-console.log(dogImageUrl)
-console.log(catImageUrl)
+
     return (
         <main className="relative bg-slate-100 md:pt-32 pb-32 pt-32 h-screen">
             <Navbar />
