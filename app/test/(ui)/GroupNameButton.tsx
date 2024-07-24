257fb21e4d1bead6d7a12135a138ec3ac12e6323
Ruby <rruby6120tw@gmail.com>
2024-07-24 14:24:08 +0800

    feat: group name setting

--- app/test/(ui)/GroupNameButton.tsx
+++ app/test/(ui)/GroupNameButton.tsx
@@ -2,7 +2,7 @@
 //import from next & react
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 //import ui
 import { CameraIcon, groupIconMap } from '@/app/test/(ui)/Icons';
 //import other
@@ -25,14 +25,18 @@ export default function GroupNameButton({
   const [currentName, setCurrentName] = useState(name);
   const [lastSavedName, setLastSavedName] = useState<any>(currentName);
   const [isShow, setIsShow] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
 
   if (!groupData) return;
 
   const toggleDialog = () => {
-    setTimeout(() => {
-      setIsShow(true);
-    }, 0);
+     setIsShow(true);
+     setTimeout(() => {
+       if (inputRef.current) {
+         inputRef.current.focus();
+       }
+     }, 0);
     router.refresh();
   };
 
@@ -66,7 +70,7 @@ export default function GroupNameButton({
       </div>
       <div
         className={clsx(
-          'fixed left-0 m-0 flex h-fit w-full flex-col justify-center items-center rounded-lg bg-transparent transition-all duration-200',
+          'fixed left-0 m-0 flex h-fit w-full flex-col items-center justify-center rounded-lg bg-transparent transition-all duration-200',
           {
             'top-0 z-50 opacity-100': isShow,
             'top-5 -z-50 opacity-0': !isShow,
@@ -81,18 +85,22 @@ export default function GroupNameButton({
           handleRightClick={() => {}}
         />
         <input
+          ref={inputRef}
           className="relative top-[92px] z-40 mx-auto w-[80%] border-0 border-b border-b-white bg-transparent px-0 text-xl text-white focus:border-b focus:border-highlight-20 focus:outline-none focus:ring-0"
           type="text"
           value={currentName}
           onChange={handleChange}
         />
         <div
-          className="fixed top-56 left-[50%] z-40 mx-auto w-[80%] rounded-full bg-highlight-20 py-3 text-center translate-x-[-50%]"
+          className="fixed left-[50%] top-56 z-40 mx-auto w-[80%] translate-x-[-50%] rounded-full bg-highlight-20 py-3 text-center"
           onClick={handleSave}
         >
           儲存
         </div>
-        <div className="absolute top-0 z-20 h-screen w-full bg-highlight-50 opacity-[95%]"></div>
+        <div
+          onClick={handleClose}
+          className="absolute top-0 z-20 h-screen w-full bg-highlight-50 opacity-[95%]"
+        ></div>
       </div>
     </div>
   );
