82c1a3ca21a5249bde3f1ac7a2548b7f87224d90
Ruby <rruby6120tw@gmail.com>
2024-08-09 22:37:45 +0800

    feat: modified details

--- app/test/split/1forTestOnly/page.tsx
+++ app/test/split/1forTestOnly/page.tsx
@@ -6,136 +6,151 @@ import SharerAmountInput from '../../(ui)/SharerAmountInput';
 import DatePickerButton from '@/app/test/(ui)/DatePickerButton';
 import ExpenseCategoryButton from '@/app/test/(ui)/ExpenseCategoryButton';
 import NoteButton from '../../(ui)/NoteButton';
-import { NextStepButton, GroupInfoBar } from '@/app/test/(ui)/ExpenseSettingDetails';
+import {
+  NextStepButton,
+  GroupInfoBar,
+} from '@/app/test/(ui)/ExpenseSettingDetails';
 import SharersAmountButton from '../../(ui)/SharersAmountButton';
 import { ExpenseSettingStepThree } from '@/app/test/(ui)/ExpenseSettingStepThree';
 import GroupPictureButton from '../../(ui)/GroupPictureButton';
 import GroupNameButton from '../../(ui)/EditGroupNameButton';
 import { GroupUsersSetting } from '../../(ui)/GroupSettingDetails';
-
+import {
+  ExtendedExpense,
+  ExtendedGroup,
+  Group,
+} from '../../(data)/(sharedFunction)/types';
 
 export default function Page() {
-  const group = {
-    id: "g2",
-    name: "2024 Japan",
-    picture: "groupIcon02",
-    creatorId: "u1",
+  const group: ExtendedGroup = {
+    id: 'g2',
+    name: '2024 Japan',
+    picture: 'groupIcon02',
+    creatorId: 'u1',
     expenses: [
       {
-        id: "e17",
-        name: "plane ticket",
+        id: 'e17',
+        name: 'plane ticket',
         amount: 180000,
-        date: "2024-05-28T00:00:00.000Z",
-        category: "transport",
-        payerId: "u1",
+        date: '2024-05-28T00:00:00.000Z',
+        category: 'transport',
+        payerId: 'u1',
         sharers: [
           {
-            id: "u1",
-            amount: 30000
+            id: 'u1',
+            amount: 30000,
           },
           {
-            id: "u2",
-            amount: 30000
+            id: 'u2',
+            amount: 30000,
           },
           {
-            id: "u3",
-            amount: 30000
+            id: 'u3',
+            amount: 30000,
           },
           {
-            id: "u4",
-            amount: 30000
+            id: 'u4',
+            amount: 30000,
           },
           {
-            id: "u5",
-            amount: 30000
+            id: 'u5',
+            amount: 30000,
           },
           {
-            id: "u6",
-            amount: 30000
-          }
+            id: 'u6',
+            amount: 30000,
+          },
         ],
-        note: ""
-      }
+        note: '',
+      },
     ],
     users: [
       {
-        id: "u1",
-        name: "a",
-        picture: "https://cdn2.thecatapi.com/images/a4v.jpg"
+        id: 'u1',
+        name: 'a',
+        picture: 'https://cdn2.thecatapi.com/images/a4v.jpg',
+        adoptable: false,
       },
       {
-        id: "u2",
-        name: "b",
-        picture: "https://images.dog.ceo/breeds/spaniel-welsh/n02102177_803.jpg"
+        id: 'u2',
+        name: 'b',
+        picture:
+          'https://images.dog.ceo/breeds/spaniel-welsh/n02102177_803.jpg',
+        adoptable: false,
       },
       {
-        id: "u3",
-        name: "c",
-        picture: "https://cdn2.thecatapi.com/images/O7FnoegHR.jpg"
+        id: 'u3',
+        name: 'c',
+        picture: 'https://cdn2.thecatapi.com/images/O7FnoegHR.jpg',
+        adoptable: false,
       },
       {
-        id: "u4",
-        name: "d",
-        picture: "https://images.dog.ceo/breeds/terrier-sealyham/n02095889_1044.jpg"
+        id: 'u4',
+        name: 'd',
+        picture:
+          'https://images.dog.ceo/breeds/terrier-sealyham/n02095889_1044.jpg',
+        adoptable: false,
       },
       {
-        id: "u5",
-        name: "e",
-        picture: "https://cdn2.thecatapi.com/images/cib.jpg"
+        id: 'u5',
+        name: 'e',
+        picture: 'https://cdn2.thecatapi.com/images/cib.jpg',
+        adoptable: false,
       },
       {
-        id: "u6",
-        name: "f",
-        picture: "https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg"
-      }
-    ]
-  }
-  const [currentExpense, setCurrentExpense] = useState({
-    id: "e17",
-    groupId: "g2",
-    name: "plane ticket",
+        id: 'u6',
+        name: 'f',
+        picture:
+          'https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg',
+        adoptable: false,
+      },
+    ],
+  };
+  const [currentExpense, setCurrentExpense] = useState<ExtendedExpense>({
+    id: 'e17',
+    // groupId: "g2",
+    name: 'plane ticket',
     amount: 180000,
-    date: "2024-05-28T08:18:15.063Z",
-    category: "transport",
-    creatorId: "u1",
-    payerId: "u1",
+    date: '2024-05-28T08:18:15.063Z',
+    category: 'transport',
+    creatorId: 'u1',
+    payerId: 'u1',
     sharers: [
       {
-        id: "u1",
-        amount: 30000
+        id: 'u1',
+        amount: 30000,
       },
       {
-        id: "u2",
-        amount: 30000
+        id: 'u2',
+        amount: 30000,
       },
       {
-        id: "u3",
-        amount: 30000
+        id: 'u3',
+        amount: 30000,
       },
       {
-        id: "u4",
-        amount: 30000
+        id: 'u4',
+        amount: 30000,
       },
       {
-        id: "u5",
-        amount: 30000
+        id: 'u5',
+        amount: 30000,
       },
       {
-        id: "u6",
-        amount: 30000
-      }
+        id: 'u6',
+        amount: 30000,
+      },
     ],
-    note: "",
-    createAt: "2024-05-28T08:18:15.063Z",
-    updateAt: "2024-05-28T08:18:15.063Z",
-    historys: [
-      {
-        editedAt: "2024-05-28T08:18:15.063Z",
-        editorId: "u1"
-      }
-    ]
+    note: '',
+    createAt: '2024-05-28T08:18:15.063Z',
+    updateAt: '2024-05-28T08:18:15.063Z',
   });
-  const [currentGroup, setCurrentGroup] = useState();
+  const [currentGroup, setCurrentGroup] = useState<Group>({
+    id: '',
+    name: '',
+    picture: 'groupIcon01',
+    users: [],
+  });
   const [phase, setPhase] = useState(3);
   const [updatedSharers, setUpdatedSharers] = useState([
     ...currentExpense.sharers,
@@ -226,6 +241,13 @@ export default function Page() {
         <GroupUsersSetting
           groupData={group}
           setCurrentGroup={setCurrentGroup}
+          isAddPage={false}
+          loginUserData={{
+            id: '',
+            name: '',
+            picture: '',
+            adoptable: false,
+          }}
         />
       </div>
     </div>
